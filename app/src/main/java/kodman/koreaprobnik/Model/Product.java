79be7a24d79f702497f452915236c5a1657d419e
CodmanD <korovin.dmitry.1977@gmail.com>
2018-03-23 18:56:27 +0300

    begin

---
+++ app/src/main/java/kodman/koreaprobnik/Model/Product.java
@@ -0,0 +1,63 @@
+package kodman.koreaprobnik.Model;
+
+/**
+ * Created by DI1 on 23.03.2018.
+ */
+
+public class Product {
+
+    private String id;
+    private String title;
+    private String category;
+    private String description;
+    private String pathImage;
+    private float price;
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPathImage() {
+        return pathImage;
+    }
+
+    public void setPathImage(String pathImage) {
+        this.pathImage = pathImage;
+    }
+}
---
+++ app/src/main/java/kodman/koreaprobnik/Util/Cnst.java
@@ -0,0 +1,14 @@
+package kodman.koreaprobnik.Util;
+
+/**
+ * Created by DI1 on 23.03.2018.
+ */
+
+public  final class Cnst {
+
+    public static String TAG="TAG";
+
+    public  static final int RC_SIGN_IN = 9001;
+    public  static final String Email = "Email";
+
+}
